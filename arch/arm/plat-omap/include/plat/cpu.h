99e70439c3523434225e8c2f1c57585e00c2e8b9
Benoit Cousson <b-cousson@ti.com>
2011-06-03 17:39:41 +0200

    OMAP4: hwmod data: Replace CHIP_IS_OMAP4430 by OMAP44XX
    In order to make hwmod data usable for all OMAP4430 variants,
    rename chip macro CHIP_IS_OMAP44XX.
    
    Create the macro as well in cpu.h
    
    Signed-off-by: Benoit Cousson <b-cousson@ti.com>
    Cc: Paul Walmsley <paul@pwsan.com>

--- arch/arm/plat-omap/include/plat/cpu.h
+++ arch/arm/plat-omap/include/plat/cpu.h
@@ -447,6 +447,8 @@ IS_OMAP_TYPE(3517, 0x3517)
 					 CHIP_IS_OMAP4430ES2_1 |	\
 					 CHIP_IS_OMAP4430ES2_2)
 
+#define CHIP_IS_OMAP44XX		(CHIP_IS_OMAP4430)
+
 /*
  * "GE" here represents "greater than or equal to" in terms of ES
  * levels.  So CHIP_GE_OMAP3430ES2 is intended to match all OMAP3430
