5f0659b64fe1d959b11347d9154947ffa5ee9ed8
Vishwanath BS <vishwanath.bs@ti.com>
2011-11-10 10:37:39 +0530

    OMAP3 PRM: Make omap3_trigger_wuclk_ctrl inline
    Make omap3_trigger_wuclk_ctrl to avoid multiply defined symbols error.
    Also remove disable_daisychain function as it's no longer being used.
    
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/mach-omap2/prm2xxx_3xxx.h
+++ arch/arm/mach-omap2/prm2xxx_3xxx.h
@@ -294,17 +294,10 @@ static inline int omap2_prm_deassert_hardreset(s16 prm_mod, u8 rst_shift,
 		"not suppose to be used on omap4\n");
 	return 0;
 }
-extern void omap3_trigger_wuclk_ctrl(void)
+extern inline void omap3_trigger_wuclk_ctrl(void)
 {
 	WARN(1, "prm: omap2xxx/omap3xxx specific function and "
 		"not suppose to be used on omap4\n");
-	return 0;
-}
-extern void omap3_disable_io_chain(void)
-{
-	WARN(1, "prm: omap2xxx/omap3xxx specific function and "
-		"not suppose to be used on omap4\n");
-	return 0;
 }
 #else
 /* Power/reset management domain register get/set */
@@ -320,7 +313,6 @@ extern int omap2_prm_is_hardreset_asserted(s16 prm_mod, u8 shift);
 extern int omap2_prm_assert_hardreset(s16 prm_mod, u8 shift);
 extern int omap2_prm_deassert_hardreset(s16 prm_mod, u8 rst_shift, u8 st_shift);
 extern void omap3_trigger_wuclk_ctrl(void);
-extern void omap3_disable_io_chain(void);
 
 /* OMAP3-specific VP functions */
 u32 omap3_prm_vp_check_txdone(u8 vp_id);
