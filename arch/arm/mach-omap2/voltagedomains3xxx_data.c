972afd4a2d1416f130f35dd61e27a6e64a7e7d65
Mike Turquette <mturquette@ti.com>
2012-02-22 22:56:21 +0800

    omap3+: voltage: add ABB data to voltage domains
    This patch adds struct omap_abb_instance to struct voltagedomain and
    populates the data for those voltage domains that have an ABB ldo on
    both 36xx and 44xx silicon.
    
    Signed-off-by: Mike Turquette <mturquette@ti.com>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- arch/arm/mach-omap2/voltagedomains3xxx_data.c
+++ arch/arm/mach-omap2/voltagedomains3xxx_data.c
@@ -26,6 +26,7 @@
 #include "voltage.h"
 #include "vc.h"
 #include "vp.h"
+#include "abb.h"
 
 /*
  * VDD data
@@ -112,6 +113,7 @@ void __init omap3xxx_voltagedomains_init(void)
 	if (cpu_is_omap3630()) {
 		omap3_voltdm_mpu.volt_data = omap36xx_vddmpu_volt_data;
 		omap3_voltdm_core.volt_data = omap36xx_vddcore_volt_data;
+		omap3_voltdm_mpu.abb = &omap36xx_abb_mpu;
 	} else {
 		omap3_voltdm_mpu.volt_data = omap34xx_vddmpu_volt_data;
 		omap3_voltdm_core.volt_data = omap34xx_vddcore_volt_data;
