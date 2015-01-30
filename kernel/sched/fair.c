2cb462bfd085bd1b205208d57a88fb8e34603bb1
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-01-30 12:16:38 +0000

    sched: Make usage tracking cpu scale-invariant
    Besides the existing frequency scale-invariance correction factor, apply
    cpu scale-invariance correction factor to usage tracking.
    
    Cpu scale-invariance takes cpu performance deviations due to
    micro-architectural differences (i.e. instructions per seconds) between
    cpus in HMP systems (e.g. big.LITTLE) and differences in the frequency
    value of the highest OPP between cpus in SMP systems into consideration.
    
    Each segment of the sched_avg::running_avg_sum geometric series is now
    scaled by the cpu performance factor too so the
    sched_avg::utilization_avg_contrib of each entity will be invariant from
    the particular cpu of the HMP/SMP system it is gathered on.
    
    So the usage level that is returned by get_cpu_usage stays relative to
    the max cpu performance of the system.
    
    In contrast to usage, load (sched_avg::runnable_avg_sum) is currently
    not considered to be made cpu scale-invariant because this will have a
    negative effect on the the existing load balance code based on
    s[dg]_lb_stats::avg_load in overload scenarios.
    
    example: 7 always running tasks
             4 on cluster 0 (2 cpus w/ cpu_capacity=512)
             3 on cluster 1 (1 cpu w/ cpu_capacity=1024)
    
                     cluster 0     cluster 1
    
    capacity         1024 (2*512)  1024 (1*1024)
    load             4096          3072
    cpu-scaled load  2048          3072
    
    Simply using cpu-scaled load in the existing lb code would declare
    cluster 1 busier than cluster 0, although the compute capacity budget
    for one task is higher on cluster 1 (1024/3 = 341) than on cluster 0
    (2*512/4 = 256).
    
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    (cherry picked from commit 448bdf8eaab1bc52d49a5822e26503353b1c7b02)
    Signed-off-by: Gaku Inami <gaku.inami.xw@bp.renesas.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2555,6 +2555,7 @@ static __always_inline int __update_entity_runnable_avg(u64 now, int cpu,
 	u32 runnable_contrib, scaled_runnable_contrib;
 	int delta_w, scaled_delta_w, decayed = 0;
 	unsigned long scale_freq = arch_scale_freq_capacity(NULL, cpu);
+	unsigned long scale_cpu = arch_scale_cpu_capacity(NULL, cpu);
 
 	delta = now - sa->last_runnable_update;
 	/*
@@ -2591,6 +2592,10 @@ static __always_inline int __update_entity_runnable_avg(u64 now, int cpu,
 
 		if (runnable)
 			sa->runnable_avg_sum += scaled_delta_w;
+
+		scaled_delta_w *= scale_cpu;
+		scaled_delta_w >>= SCHED_CAPACITY_SHIFT;
+
 		if (running)
 			sa->running_avg_sum += scaled_delta_w;
 		sa->avg_period += delta_w;
@@ -2615,6 +2620,10 @@ static __always_inline int __update_entity_runnable_avg(u64 now, int cpu,
 
 		if (runnable)
 			sa->runnable_avg_sum += scaled_runnable_contrib;
+
+		scaled_runnable_contrib *= scale_cpu;
+		scaled_runnable_contrib >>= SCHED_CAPACITY_SHIFT;
+
 		if (running)
 			sa->running_avg_sum += scaled_runnable_contrib;
 		sa->avg_period += runnable_contrib;
@@ -2625,6 +2634,10 @@ static __always_inline int __update_entity_runnable_avg(u64 now, int cpu,
 
 	if (runnable)
 		sa->runnable_avg_sum += scaled_delta;
+
+	scaled_delta *= scale_cpu;
+	scaled_delta >>= SCHED_CAPACITY_SHIFT;
+
 	if (running)
 		sa->running_avg_sum += scaled_delta;
 	sa->avg_period += delta;
