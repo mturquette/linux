37288f6e98a919128ce4b8daf55252abb0602081
Michael Turquette <mturquette@linaro.org>
2015-04-06 21:16:54 -0700

    sched: export get_cpu_usage & capacity_of in sched.h
    Both of these functions are useful to cpu frequency scaling code which
    is based on the CFS load tracking and cpu capacity metrics. Expose these
    calls in sched.h so that they can be used by this future policy.
    
    Signed-off-by: Michael Turquette <mturquette@linaro.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4376,7 +4376,7 @@ static unsigned long target_load(int cpu, int type)
 	return max(rq->cpu_load[type-1], total);
 }
 
-static unsigned long capacity_of(int cpu)
+unsigned long capacity_of(int cpu)
 {
 	return cpu_rq(cpu)->cpu_capacity;
 }
@@ -4816,7 +4816,7 @@ done:
  * Without capping the usage, a group could be seen as overloaded (CPU0 usage
  * at 121% + CPU1 usage at 80%) whereas CPU1 has 20% of available capacity/
  */
-static int get_cpu_usage(int cpu)
+int get_cpu_usage(int cpu)
 {
 	unsigned long usage = cpu_rq(cpu)->cfs.utilization_load_avg;
 	unsigned long capacity = capacity_orig_of(cpu);
--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1362,6 +1362,8 @@ static inline int hrtick_enabled(struct rq *rq)
 #ifdef CONFIG_SMP
 extern void sched_avg_update(struct rq *rq);
 extern unsigned long arch_scale_freq_capacity(struct sched_domain *sd, int cpu);
+int get_cpu_usage(int cpu);
+unsigned long capacity_of(int cpu);
 
 static inline void sched_rt_avg_update(struct rq *rq, u64 rt_delta)
 {
