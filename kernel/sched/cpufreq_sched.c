b6e0c990b98ac5610837ee8d5b1fb26660a9ed55
Steve Muckle <smuckle@linaro.org>
2015-11-25 15:59:25 -0800

    sched/cpufreq_sched: add trace events
    Trace events will aid in debugging, profiling and tuning.
    
    Signed-off-by: Steve Muckle <smuckle@linaro.org>

--- kernel/sched/cpufreq_sched.c
+++ kernel/sched/cpufreq_sched.c
@@ -15,6 +15,9 @@
 #include <linux/delay.h>
 #include <linux/string.h>
 
+#define CREATE_TRACE_POINTS
+#include <trace/events/cpufreq_sched.h>
+
 #include "sched.h"
 
 struct static_key __read_mostly __sched_freq = STATIC_KEY_INIT_FALSE;
@@ -94,6 +97,7 @@ static bool finish_last_request(struct gov_data *gd)
 		int usec_left = ktime_to_ns(ktime_sub(gd->throttle, now));
 
 		usec_left /= NSEC_PER_USEC;
+		trace_cpufreq_sched_throttled(usec_left);
 		usleep_range(usec_left, usec_left + 100);
 		now = ktime_get();
 		if (ktime_after(now, gd->throttle))
@@ -197,6 +201,9 @@ static void update_fdomain_capacity_request(int cpu)
 		goto out;
 	freq_new = policy->freq_table[index_new].frequency;
 
+	trace_cpufreq_sched_request_opp(cpu, capacity, freq_new,
+					gd->requested_freq);
+
 	if (freq_new == gd->requested_freq)
 		goto out;
 
@@ -234,6 +241,8 @@ void update_cpu_capacity_request(int cpu, bool request)
 
 	if (new_capacity == scr->total)
 		return;
+
+	trace_cpufreq_sched_update_capacity(cpu, request, scr, new_capacity);
 
 	scr->total = new_capacity;
 	if (request)
